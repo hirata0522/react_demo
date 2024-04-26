61f7d342b1006396446ff4c0491bdaf2f625e634
hirata <masatohirata007@gmail.com>
2024-04-26 19:46:30 +0900

    upfate files

--- src/App.js
+++ src/App.js
@@ -1,88 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import PersonIcon from '@mui/icons-material/PersonAdd';
+
+const fileName = 'jsonファイル'
+const jsonData = { a: 1, b: 2 }
+
+const fileNameWithJson = `${fileName}.json`
+const blobData = new Blob([JSON.stringify(jsonData)], {
+  type: 'text/json',
+})
+const jsonURL = URL.createObjectURL(blobData)
+
+
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // JSONファイルのパス
+    const jsonFilePath = './code.json';
+
+    // JSONファイルを取得する関数
+    const fetchData = async () => {
+      try {
+        const response = await fetch(jsonFilePath);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching JSON data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  const fileName = 'jsonファイル'
+  const jsonData = { a: 1, b: 2 }
+
+  const fileNameWithJson = `${fileName}.json`
+  const blobData = new Blob([JSON.stringify(jsonData)], {
+    type: 'text/json',
+  })
+
+  // return (
+  //     <a href={jsonURL} download={fileNameWithJson}>
+  //       エクスポート
+  //     </a>
+  //   )
+
+  return (
+    <div>
+      <h1>JSONファイルの表示</h1>
+      <PersonIcon />
+      {data ? (
+        <div>
+          <h2>{data.CD001.NAME}</h2>
+          <p>{data.CD001.SUPP}</p>
+          <h2>{data.CD002.NAME}</h2>
+          <p>{data.CD002.SUPP}</p>
+          <h2>{data.CD003.NAME}</h2>
+          <p>{data.CD003.SUPP}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+
+}
+
+export default App;
+
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 // function App() {
 //   return (
 //     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
+//       真っ白
 //     </div>
 //   );
 // }
-
-
-// import Gallery from './Gallery.js';
-// import { Profile } from './Profile.js';
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Profile />
-//       <Gallery />
-//     </div>
-//   );
-// }
-
-// const today = new Date();
-
-// function formatDate(date) {
-//   return new Intl.DateTimeFormat(
-//     'en-US',
-//     { weekday: 'long' }
-//   ).format(date);
-// }
-
-// export default function TodoList() {
-//   return (
-//     <h1>To Do List for {formatDate(today)}</h1>
-//   );
-// }
-
-
-const baseUrl = 'https://i.imgur.com/';
-const person = {
-  name: 'Gregorio Y. Zara',
-  imageId: '7vQD0fP',
-  imageSize: 's',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
-
-export default function TodoList() {
-  return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
-        alt={person.name}
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
-  );
-}
-
-
-
-
-
 
 // export default App;
